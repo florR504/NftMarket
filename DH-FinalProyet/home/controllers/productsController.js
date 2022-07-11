555f191530d5152dea5c2d647f82774f183faecf
Nico9024 <delfiolnm@gmail.com>
2022-07-11 18:10:43 -0300

    agregando cambios al responsive

--- DH-FinalProyet/home/controllers/productsController.js
+++ DH-FinalProyet/home/controllers/productsController.js
@@ -9,44 +9,19 @@ const productos = {
     res.render("coleccion", { products });
   },
 
-  detalle1: (req, res) => {
-    res.render("productDetail1");
+  detalle: (req, res) => {
+    const idProduct = req.params.id;
+    console.log ("ID DE PRODUCTO", idProduct);
+    const producto = products.filter (function(elemento) {
+      if (idProduct == elemento.id) {
+        return elemento;
+      }
+    });
+    console.log ("PRODUCTO", producto [0]);
+    res.render ("productDetail", {producto: producto[0]});
   },
-  detalle2: (req, res) => {
-    res.render("productDetail2");
-  },
-  detalle3: (req, res) => {
-    res.render("productDetail3");
-  },
-  detalle4: (req, res) => {
-    res.render("productDetail4");
-  },
-  detalle5: (req, res) => {
-    res.render("productDetail5");
-  },
-  detalle6: (req, res) => {
-    res.render("productDetail6");
-  },
-  detalle7: (req, res) => {
-    res.render("productDetail7");
-  },
-  detalle8: (req, res) => {
-    res.render("productDetail8");
-  },
-  detalle9: (req, res) => {
-    res.render("productDetail9");
-  },
-  detalle10: (req, res) => {
-    res.render("productDetail10");
-  },
-  detalle11: (req, res) => {
-    res.render("productDetail11");
-  },
-  detalle12: (req, res) => {
-    res.render("productDetail12");
-  },
-
-  crear: (req, res) => {
+    
+    crear: (req, res) => {
     res.render("createproduct");
   },
   store: (req, res) => {
--- DH-FinalProyet/home/routers/products.js
+++ DH-FinalProyet/home/routers/products.js
@@ -17,22 +17,10 @@ const storage = multer.diskStorage({
 var upload = multer({storage: storage});
 
 router.get('/', productsController.coleccion);
-router.get('/productDetail1', productsController.detalle1);
-router.get('/productDetail2', productsController.detalle2);
-router.get('/productDetail3', productsController.detalle3);
-router.get('/productDetail4', productsController.detalle4);
-router.get('/productDetail5', productsController.detalle5);
-router.get('/productDetail6', productsController.detalle6);
-router.get('/productDetail7', productsController.detalle7);
-router.get('/productDetail8', productsController.detalle8);
-router.get('/productDetail9', productsController.detalle9);
-router.get('/productDetail10', productsController.detalle10);
-router.get('/productDetail11', productsController.detalle11);
-router.get('/productDetail12', productsController.detalle12);
-
+router.get('/productDetail/:id', productsController.detalle);
 router.get('/createProduct', productsController.crear );
 router.post('/createProduct', upload.single("imagen"), productsController.store );
-router.get('/productDetail1/:id/edit', productsController.editForm);
-router.put('/productDetail1/:id/edit',upload.single("imagen"), productsController.editar);
+router.get('/productDetail/:id/edit', productsController.editForm);
+router.put('/productDetail/:id/edit',upload.single("imagen"), productsController.editar);
 
 module.exports = router;
