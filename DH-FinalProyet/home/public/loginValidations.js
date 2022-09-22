2afe3ffde0d35dd299ee36bb89c74785cad58a5b
Flor <florenciar504@gmail.com>
2022-09-22 20:22:47 -0300

    Validaciones del login front

---
+++ DH-FinalProyet/home/public/loginValidations.js
@@ -0,0 +1,65 @@
+window.addEventListener('load', function(){
+    //Capturo los input del form
+    let $loginEmail = document.querySelector('#usuario');
+    let $loginPassword = document.querySelector('#contraseña');
+   
+    let errors = [];//Array errors
+   
+    $loginEmail.focus(); //selecciono el input apenas carga la página
+    //valido que los campos esten completos
+    function validarVacio (event){
+     let name = event.target.name //obtengo el nombre del componente donde se triggerea el evento
+   
+     if(this.value == ''){
+ 
+       
+       errors[name] = 'No puedes dejar este campo vacio';
+ 
+       let $divError = document.querySelector('.error-'+ name); //atrapo al div correspondiente al input donde se genera el error
+    
+       $divError.classList.add('error')//asigno la clase de error al div
+ 
+       return $divError.innerHTML = errors[name];
+     }else{
+ 
+       errors[name] = '';
+       let $divError = document.querySelector('.error-'+ name);
+    
+       return $divError.innerHTML = errors[name];
+     }
+    }
+ 
+    function islengthOK(min){
+      return function(event){
+          let name = event.target.name;
+          let valueLength = this.value.length;
+        
+          if(valueLength > 0 && valueLength < min){
+             
+             errors[name] = 'El valor ingresado debe ser mas largo';
+ 
+             let $divError = document.querySelector('.error-'+ name);
+            
+             $divError.classList.add('error')
+ 
+             return $divError.innerHTML = errors[name];
+             
+       }else if( valueLength >= min){
+ 
+          errors[name] = '';
+          let $divError = document.querySelector('.error-'+ name);
+
+          return $divError.innerHTML = errors[name];
+      }
+ 
+    }
+ }
+  
+    
+    $loginEmail.addEventListener('blur', validarVacio);
+    $loginEmail.addEventListener('blur', islengthOK(2));
+   
+    $loginPassword.addEventListener('blur', validarVacio);
+    $loginPassword.addEventListener('blur', islengthOK(2));
+ 
+}) 
