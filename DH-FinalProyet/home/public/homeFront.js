c9f891e2ec00147eaedd61536982e8e9e3fa958f
Flor <florenciar504@gmail.com>
2022-10-14 14:14:51 -0300

    Sidebar adaptable segun este logueado o no el user

--- DH-FinalProyet/home/public/homeFront.js
+++ DH-FinalProyet/home/public/homeFront.js
@@ -1,35 +1,38 @@
-window.addEventListener('load', function(){
- let $sideBar = document.getElementById('mySidebar')
- let $open =  document.querySelector(".openbtn")
- let $menu =  document.getElementById('menu')
- let $close =  document.getElementById('close')
- let $user = document.getElementById('user_nav')
- let $login = document.getElementById('form_nav')
- let $register = document.getElementById('register_nav')
- let $buscador = document.getElementById('buscador_nav')
- function openNav() {
-  $sideBar.style.width = "250px";
-  $open.style.marginLeft = "250px";
-  $login.style.display = 'none';
-  $register.style.display = 'none';
-  $buscador.style.display = 'none'
- 
- }
- function closeNav() {
-  $sideBar.style.width = "0";
-  $open.style.marginLeft= "0";
-  $login.style.display = 'block';
-  $register.style.display = 'block';
-  $buscador.style.display = 'block'
-}
+window.addEventListener("load", function () {
+  let $sideBar = document.getElementById("mySidebar");
+  let $open = document.querySelector(".openbtn");
+  let $menu = document.getElementById("menu");
+  let $close = document.getElementById("close");
+  let $user = document.getElementById("user_nav");
+  let $login = document.getElementById("form_nav");
+  let $register = document.getElementById("register_nav");
+  let $buscador = document.getElementById("buscador_nav");
 
-$menu.addEventListener('click', openNav)
-$close.addEventListener('click', closeNav)
+  function openNav() {
+    $sideBar.style.width = "250px";
+    $open.style.marginLeft = "250px";
+    if ($login && $register) {
+      $login.style.display = "none";
+      $register.style.display = "none";
+      $buscador.style.display = "none";
+    } else if ($user) {
+      $user.style.display = "none";
+      $buscador.style.display = "none";
+    }
+  }
+  function closeNav() {
+    $sideBar.style.width = "0";
+    $open.style.marginLeft = "0";
+    if ($login && $register) {
+      $login.style.display = "block";
+      $register.style.display = "block";
+      $buscador.style.display = "block";
+    } else if ($user) {
+      $user.style.display = "flex";
+      $buscador.style.display = "block";
+    }
+  }
 
-
-})
-//Capturar elementos del nav y hacer que se oculten al abrir el sideBar
-
-
-
-
+  $menu.addEventListener("click", openNav);
+  $close.addEventListener("click", closeNav);
+});
